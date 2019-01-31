36f525826c60638ef849d94e14918c1b8b958cf2
hankrum <st_kolev@mail.bg>
2019-01-31 05:38:49 +0200

    tests

---
+++ OddsSystem.Test/SportEventControllerTests/Constructor_Should.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OddsSystem.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OddsSystem.Test.SportEventControllerTests
+{
+    [TestClass]
+    public class Constructor_Should
+    {
+        [TestMethod]
+        public void Throw_ArgumentNullException_When_SportEventService_Is_Null()
+        {
+            Assert.ThrowsException<ArgumentNullException>(
+                () => new SportEventController(null)
+                );
+        }
+    }
+}
---
+++ OddsSystem.Test/SportEventControllerTests/Get_Should.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OddsSystem.Data.Model;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using OddsSystem.Services.Data.Contracts;
+using Moq;
+using OddsSystem.Controllers;
+
+namespace OddsSystem.Test.SportEventControllerTests
+{
+    [TestClass]
+    public class Get_Should
+    {
+        [TestMethod]
+        public void Return_Correct_Result_When_Invoked()
+        {
+            //Arrange
+            var testDate = new DateTime(2019, 1, 1);
+            var sportEventsCollection = new SportEvent[]
+            {
+                new SportEvent
+                {
+                    EventName = "Test1"
+                },
+                new SportEvent
+                {
+                    EventName = "Test2"
+                }
+            };
+            var expectedResult = new JsonResult(sportEventsCollection);
+            var sportEventServiceMock = new Mock<ISportEventService>();
+            sportEventServiceMock.Setup(s => s.All()).Returns(sportEventsCollection);
+            var sut = new SportEventController(sportEventServiceMock.Object);
+            //Act
+            var result = sut.Get() as JsonResult;
+            //Assert
+            Assert.AreEqual(expectedResult.Value, result.Value);
+        }
+    }
+}
